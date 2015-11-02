f9a1c4f16b39a9fcbdd38310952f8dd26cf234c0
Julien Chantereau <jchantereau@gmail.com>
2015-11-02 17:32:42 +0100

    track model done

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -1,6 +1,8 @@
 class Vinyl < ActiveRecord::Base
   belongs_to :user
   has_many :bookings
+  has_many :tracks
+
   validates :title, presence: true
   validates :artist, presence: true
   validates :price, presence: true
