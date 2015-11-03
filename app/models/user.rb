e001b51a72067cc0a278a2dd119f371771db5958
Thomas Joët <thomas.joet.fr@gmail.com>
2015-11-03 14:55:44 +0100

    migrated the schema to add picture to User model + updated User model + updated user edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings
   has_many :vinyls
+  has_attached_file :picture,
+    styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
 end
