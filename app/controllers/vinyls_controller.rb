46d18bad1d8064f6c464f3e31698a272ec10236a
Julien Chantereau <jchantereau@gmail.com>
2015-11-03 13:52:58 +0100

    basic show structure

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,7 +1,18 @@
 class VinylsController < ApplicationController
 
+  before_action :find_vinyl, only: [:show]
+
   def index
     @vinyls = Vinyl.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_vinyl
+    @vinyl = Vinyl.find(params[:id])
   end
 
 end
