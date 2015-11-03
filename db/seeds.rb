1d456fdf6eb2c64f53a656b805b45989ea8661c6
Julien Chantereau <jchantereau@gmail.com>
2015-11-03 10:56:03 +0100

    seed file model created

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,22 @@
 Vinyl.destroy_all
 vinyls_attributes = [
   {
-    name:         "",
-    artist:       "",
-    picture:      "",
-    price:
+    name:         "Homesick #4",
+    artist:       "Marquis Hawkes",
+    picture:      "http://cdn.discogs.com/4_4NjcC_pk5uxh3OElBXkMCEbQ8=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(96)/discogs-images/R-7511583-1442999947-7729.jpeg.jpg",
+    price:        10
   },
   {
-    name:         "",
-    artist:       "",
-    picture:      "",
-    price:
+    name:         "More Maim / In Here",
+    artist:       "Joy Orbison & Boddika",
+    picture:      "http://cdn.discogs.com/abee6ZXaL6cko2-gEQHN38I3yg8=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-5094666-1392485050-1800.jpeg.jpg",
+    price:        10
   },
   {
-    name:         "",
-    artist:       "",
-    picture:      "",
-    price:
+    name:         "What's A Girl To Do?",
+    artist:       "Fatima Yamaha",
+    picture:      "http://cdn.discogs.com/qZP21voZ_krQpxjHfVKyXvRuGmA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-7102377-1433782866-7012.jpeg.jpg",
+    price:        10
   },
   {
     name:         "",
