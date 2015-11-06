8ecd72ed2470bee4175e0b529c27b49387ee8462
Julien Chantereau <jchantereau@gmail.com>
2015-11-06 10:56:54 +0100

    better navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
+User.where(email: "test@test.com").destroy_all
 Booking.destroy_all
 Vinyl.destroy_all
+
+user = User.create(email: "test@test.com", password: "password", password_confirmation: "password")
 vinyls_attributes = [
   {
     title:        "Homesick #4",
     artist:       "Marquis Hawkes",
     picture:      "http://cdn.discogs.com/4_4NjcC_pk5uxh3OElBXkMCEbQ8=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(96)/discogs-images/R-7511583-1442999947-7729.jpeg.jpg",
     price:        10,
-    user_id:      1,
+    user_id:      user.id,
     address:      "10 Rue Crespin du Gast, Paris"
   },
   {
@@ -14,7 +17,7 @@ vinyls_attributes = [
     artist:       "Joy Orbison & Boddika",
     picture:      "http://cdn.discogs.com/abee6ZXaL6cko2-gEQHN38I3yg8=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-5094666-1392485050-1800.jpeg.jpg",
     price:        10,
-    user_id:      1,
+    user_id:      user.id,
     address:      "20 Rue Crespin du Gast, Paris"
   },
   {
@@ -22,7 +25,7 @@ vinyls_attributes = [
     artist:       "Fatima Yamaha",
     picture:      "http://cdn.discogs.com/qZP21voZ_krQpxjHfVKyXvRuGmA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-7102377-1433782866-7012.jpeg.jpg",
     price:        10,
-    user_id:      1,
+    user_id:      user.id,
     address:      "10 Rue oberkampf, Paris"
   },
   {
@@ -30,7 +33,7 @@ vinyls_attributes = [
     artist:       "Simo Cell",
     picture:      "http://cdn.discogs.com/jN6rdPlJCJTjIACkT8rf6vw8G9g=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-7087294-1442391121-6285.jpeg.jpg",
     price:        7,
-    user_id:      1,
+    user_id:      user.id,
     address:      "102 Rue oberkampf, Paris"
   },
   {
@@ -38,7 +41,7 @@ vinyls_attributes = [
     artist:       "Nthng",
     picture:      "http://cdn.discogs.com/x_hxJSudaXowxsre5z_1sAhy6Gs=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-6939574-1438587640-7055.jpeg.jpg",
     price:        10,
-    user_id:      1,
+    user_id:      user.id,
     address:      "202 Rue oberkampf, Paris"
   },
   {
@@ -46,7 +49,7 @@ vinyls_attributes = [
     artist:       "Floatingpoints",
     picture:      "http://cdn.discogs.com/ev-24GSwbPgkTZB3V6RGnldS0rA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-1912828-1296284515.jpeg.jpg",
     price:        7,
-    user_id:      1,
+    user_id:      user.id,
     address:      "3 Rue oberkampf, Paris"
   },
   {
@@ -54,7 +57,7 @@ vinyls_attributes = [
     artist:       "Sound Stream",
     picture:      "http://cdn.discogs.com/nDz-64HivMrQv_b2JrediM3RS-g=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-1433650-1219368117.gif.jpg",
     price:        10,
-    user_id:      1,
+    user_id:      user.id,
     address:      "5 Av. des Champs-Élysées, Paris"
   },
   {
@@ -62,7 +65,7 @@ vinyls_attributes = [
     artist:       "Dungeon Meat / SE62",
     picture:      "http://cdn.discogs.com/l-_DSKu3RglHipEaWrmX--tpQbM=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-4895692-1380547201-3850.jpeg.jpg",
     price:        10,
-    user_id:      1,
+    user_id:      user.id,
     address:      "100 Av. des Champs-Élysées, Paris"
   },
   {
@@ -70,7 +73,7 @@ vinyls_attributes = [
     artist:       "Aphex Twin",
     picture:      "http://cdn.discogs.com/G7K6S5pr359EWwFC6kt-BDVj7Nc=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-4887177-1378982124-2981.jpeg.jpg",
     price:        18,
-    user_id:      1,
+    user_id:      user.id,
     address:      "33 Av. des Champs-Élysées, Paris"
   },
   {
@@ -78,7 +81,7 @@ vinyls_attributes = [
     artist:       "Frankie Knuckles",
     picture:      "http://cdn.discogs.com/1K4C0Ub83MVUGJQqOfWJIPjuJ7E=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-17670-1283938764.jpeg.jpg",
     price:        2,
-    user_id:      1,
+    user_id:      user.id,
     address:      "76 Av. des Champs-Élysées, Paris"
   }
 ]
